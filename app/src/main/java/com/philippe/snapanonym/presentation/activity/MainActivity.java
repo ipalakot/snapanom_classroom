d933f85543fa67284168a13c948397f60d6d8324
imphilippesimo <philippechampion58@gmail.com>
2019-03-18 14:52:37 +0100

    splash screen

---
+++ app/src/main/java/com/philippe/snapanonym/presentation/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.philippe.snapanonym.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.philippe.snapanonym.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onExploreSnaps(View view) {
+    }
+
+    public void onPublishSnap(View view) {
+    }
+}
