897f9ffa5984427758db1d7f9c97e1d394f2f976
imphilippesimo <philippechampion58@gmail.com>
2019-03-18 16:58:36 +0100

    create main layout and activity and create snap item layout

--- app/src/main/java/com/philippe/snapanonym/presentation/activity/MainActivity.java
+++ app/src/main/java/com/philippe/snapanonym/presentation/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.philippe.snapanonym.presentation.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.philippe.snapanonym.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onExploreSnaps(View view) {
+        Intent intent = new Intent(this,SnapListActivity.class);
+        startActivity(intent);
+
     }
 
     public void onPublishSnap(View view) {
